23fce480c484d42eb964698a467a04186f2f0972
Esauflorz24 <102836833+Esauflorz24@users.noreply.github.com>
2022-12-09 20:30:38 -0600

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-nitrogen --restore &
 udiskie -t &
-volumeicon &
 nm-applet &
 cbatticon -u 5 &
-setxkbmap -layout us -variant altgr-intl -option nodeadkeys
-picom &
 #picom --config=$HOME/.config/picom/picom.conf &
 
