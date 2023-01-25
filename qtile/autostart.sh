9760f3c4d435f380d1b55dafffc80460f9dd4553
E$Z <102836833+Esauflorz24@users.noreply.github.com>
2023-01-25 17:52:52 -0600

    Update autostart.sh

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+# usb application
 udiskie -t &
-nm-applet &
-cbatticon -u 5 &
-#picom --config=$HOME/.config/picom/picom.conf &
 
+# network manager applet
+nm-applet &
+
+# volume icon
+volumeicon &
+
+# wallpapper
+nitrogen --restore &
